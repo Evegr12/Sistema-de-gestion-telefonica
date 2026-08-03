23acb5dc803217b675e287a1717910af83cca9e7
Essa-Cortez <essawcortez@outlook.com>
2026-08-03 12:32:44 -0600

    Essaw brandh updated

--- src/com/tuuniversidad/agenda/modelos/Contacto.java
+++ src/com/tuuniversidad/agenda/modelos/Contacto.java
@@ -1,4 +1,74 @@
 package com.tuuniversidad.agenda.modelos;
 
+import java.util.Objects;
+
 public class Contacto {
+
+    //Atributos
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    //constructor
+
+    public Contacto(String nombre, String apellido, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+
+    //METODO GET Y SET
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    //Validación requisito cuando un contacto es igual a otro o cuando sus nombres son iguales
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Contacto contacto = (Contacto) obj;
+        return nombre.equalsIgnoreCase(contacto.nombre) && apellido.equalsIgnoreCase(contacto.apellido);
+    }
+
+
+    //Hash Code -- convertir nombre en minuscula --
+    @Override
+    public int hashCode(){
+        return Objects.hash(nombre.toLowerCase(), apellido.toLowerCase());
+    }
+
+
+    //METODO toString
+
+    public String toString(String nombre){
+        return "Nombre: " + nombre + " " + apellido + "Telefono: "  + telefono;
+    }
+
+
 }
