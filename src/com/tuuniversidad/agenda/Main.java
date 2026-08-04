1b86dadeffc14033dbb7b235273a694d01fe5092
ericperucho <ericperucho12@gmail.com>
2026-08-03 19:33:34 -0600

    Fix: Restaurar el soporte de comandos CLI en la carpeta tuuniversidad

--- src/com/tuuniversidad/agenda/Main.java
+++ src/com/tuuniversidad/agenda/Main.java
@@ -1,14 +1,19 @@
 package com.tuuniversidad.agenda;
 
 import com.tuuniversidad.agenda.ui.Menu;
+import com.tuuniversidad.agenda.ui.CLI;
 
 public class Main {
     public static void main(String[] args) {
-
-        Menu menuAgenda = new Menu();
-
-        //Llamamos al metodo de menu
-
-        menuAgenda.iniciar();
+        // Validamos si el usuario ingresó comandos desde la terminal al ejecutar el archivo
+        if (args.length > 0) {
+            // Modo CLI (Command Line Interface)
+            CLI consolaExterna = new CLI();
+            consolaExterna.procesarArgumentos(args);
+        } else {
+            // Modo Interactivo (Menú visual en consola)
+            Menu menuAgenda = new Menu();
+            menuAgenda.iniciar();
+        }
     }
 }
